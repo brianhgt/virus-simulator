f47d0f17d8f848c838bc7ccab5a169d7b658a13f
brianhgt <brian.h.gt@gmail.com>
2020-03-31 11:29:37 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,17 @@ export const BOX_SIZE = 300;
 export const HISTORY = 400;
 export const PEOPLE = 300;
 export const SIMULATOR_SIZE = 500;
+
+
+export const QUARINTINE_SIZE = 10;
+export const QUARINTINE_SITES = 0;
+
+export const TRAFFIC_HUBS = 0;
+export const TRAFFIC_HUB_SIZE = 5;
+export const TRAFFIC_HUB_WORKERS = 5;
+
+export const COMUNITIES = 0;
+export const COMUNITY_TRAVEL_RATE = 0.02;
+
+export const HERD_INITIAL_POPULATION = 0.01;
+export const HERD_IMPL_TIME = 0.001;
